d10131931c44340ebb4e4227e3dd7b9185c52f2c
sergo <elhom99@gmail.com>
2022-07-24 18:02:09 +0300

    update html

--- pr_1/pages/views.py
+++ pr_1/pages/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse('hello')
-
-
 def about(request):
+    return render(request, 'about.html')
+
+
+def main(request):
     return render(request, 'main.html')
